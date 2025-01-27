4f6061414b69d6b68355bd7197826017ccc8f876
muttu <mutturajvittal998@gmail.com>
2025-01-27 23:12:42 +0530

    Uploading files

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./Footer.scss"
+
+const Footer = () => {
+    return (
+        <footer className="footer">
+            <p>Made with <span role="img" aria-label="heart">❤️</span> by Muttu Handigund</p>
+        </footer>
+    )
+}
+
+export default Footer
