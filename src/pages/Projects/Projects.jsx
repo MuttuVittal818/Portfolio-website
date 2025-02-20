b54c21098794aebaf956b3cc3dfb471a9c325d72
muttu <mutturajvittal998@gmail.com>
2025-02-20 16:15:16 +0530

    Changes Done

--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import './Projects.scss'
+import React from 'react';
+import './Projects.scss';
 
-import Lottie from 'lottie-react';
-import project from '../../assets/project.json'
-import OpenInNewSharpIcon from '@mui/icons-material/OpenInNewSharp';
 import GitHubIcon from '@mui/icons-material/GitHub';
+import OpenInNewSharpIcon from '@mui/icons-material/OpenInNewSharp';
+import Lottie from 'lottie-react';
+import project from '../../assets/project.json';
 
 const Projects = () => {
   return (
@@ -34,16 +34,16 @@ const Projects = () => {
               <div className="project-work-header">
                 <div className="project-work-heading">
                   <div className="project-work-title">
-                    <h2>ArtBaba</h2>
-                    <h3>Full Stack website</h3>
+                    <h2>QR-Codes Generater & Reader</h2>
+                    <h3>Simple Static website</h3>
                   </div>
 
                 </div>
                 <div className="project-work-duration">
                   <h3>June, 2023</h3>
                   <div className="project-work-links">
-                    <a href="https://artbaba.vercel.app/"><OpenInNewSharpIcon /></a>
-                    <a href="https://github.com/rakshithr0177/artbaba"><GitHubIcon /></a>
+                    <a href="https://qr-codes-generater-and-reader-web-project.vercel.app/"><OpenInNewSharpIcon /></a>
+                    <a href="https://github.com/MuttuVittal818/QR-Codes-Generater-and-Reader-Web-Project"><GitHubIcon /></a>
                   </div>
                 </div>
               </div>
@@ -51,36 +51,24 @@ const Projects = () => {
               <div className="project-work-body">
                 <ul>
                   <li>⚡
-                    <p>Tech Stack: React.js, Express.js, Node.js, MongoDB, CSS.</p>
+                    <p>Tech Stack: HTML, CSS, JavaScript, Libraries</p>
                   </li>
                   <li>⚡
-                    <p>Developed a blogging platform designed to showcase artists' talents using the MERN stack.</p>
+                    <p>Developed a QR code website with generator and reader functionalities using qrcode.js and html5-qrcode.</p>
                   </li>
                   <li>⚡
-                    <p>Implemented user authentication and authorization with JSON Web Tokens (JWT).</p>
+                    <p>Managed Designed a user-friendly interface supporting text input and image upload for scanning.</p>
                   </li>
                   <li>⚡
-                    <p>Created a well-structured menu to navigate the home page, content, create posts, and integrated like/dislike functionality for each post.</p>
+                    <p>Created a well-structured menu for navigating the home page, generating QR codes, scanning QR codes, uploading images, and additional options for setting up the QR code.</p>
                   </li>
                   <li>
                     <div className="icons">
-                      <img className="icon" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg" />
+                      <img className="icon" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original.svg" />
 
-                      <img className="icon" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nodejs/nodejs-original-wordmark.svg" />
+                      <img className="icon" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/css3/css3-original.svg" />
 
-                      <img className="icon" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mongodb/mongodb-original.svg" />
-
-
-                      <img className="icon" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/sass/sass-original.svg" />
-
-                      <img className="icon" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/postman/postman-original.svg" />
-
-                      <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/redux/redux-original.svg" />
-
-
-
-                      <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/css3/css3-original.svg" />
-
+                      <img className="icon" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg" />
 
 
                     </div>
@@ -103,10 +91,10 @@ const Projects = () => {
 
                 </div>
                 <div className="project-work-duration">
-                  <h3>January, 2023</h3>
+                  <h3>January, 2022</h3>
                   <div className="project-work-links">
-                    <a href="https://react-sorting-algo.vercel.app/"><OpenInNewSharpIcon /></a>
-                    <a href="https://github.com/rakshithr0177/react-sorting-algo"><GitHubIcon /></a>
+                    <a href="https://sortingvisualizerwebapplication.vercel.app/"><OpenInNewSharpIcon /></a>
+                    <a href="https://github.com/MuttuVittal818/Fullstack_Web"><GitHubIcon /></a>
                   </div>
                 </div>
               </div>
